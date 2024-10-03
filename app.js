a1a73626a28f75cf0a4b4dd03bd6662bf8099e7c
GOrtiz2305 <160190916+GOrtiz2305@users.noreply.github.com>
2024-10-03 11:20:35 -0600

    More fixing

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader(
       "Access-Control-Allow-Origin",
-      "https://ortizdev.xyz"
+      "https://ortizdev.xyz",
+        "http://localhost:3000",
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
