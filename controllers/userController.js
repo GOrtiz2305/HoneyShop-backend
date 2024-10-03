c7a96a04cc82fcf1e7f0e8e8942a474564aa7b57
GOrtiz2305 <160190916+GOrtiz2305@users.noreply.github.com>
2024-10-03 17:07:35 -0600

    Token time

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ module.exports = {
             const { email, password } = req.body;
             const user = await userModel.findOne({ where: { email, password } });
             if (user) {
-                const token = jwt.sign({ user }, 'secretkey', { expiresIn: '1m' });
+                const token = jwt.sign({ user }, 'secretkey', { expiresIn: '5m' });
                 res.json({ token });
             } else {
                 res.status(401).json({ error: "Usuario no encontrado" });
